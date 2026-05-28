fed66a57b0314d5ba19268f575f6a7a9e71ab461
MenaceHecker <thedynamic2508@gmail.com>
2026-05-27 22:05:00 -0400

    Initial Commit

---
+++ PixelVault/PixelVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelVaultApp.swift
+//  PixelVault
+//
+//  Created by Tushar Mishra on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PixelVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
