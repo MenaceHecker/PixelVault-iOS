283abb05f6e64cf807ad0e0fe7adcb84ee1174eb
Tushar Mishra <thedynamic2508@gmail.com>
2026-06-01 09:43:36 -0400

    Added requestPermission and fetchAssetCount func

--- PixelVault/PhotoLibraryService.swift
+++ PixelVault/PhotoLibraryService.swift
@@ -4,4 +4,28 @@
 //
 //  Created by Tushar Mishra on 6/1/26.
 //
+import Photos
 
+final class PhotoLibraryService {
+
+    func requestPermission() async -> Bool {
+        let currentStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+
+        switch currentStatus {
+        case .authorized, .limited:
+            return true
+
+        case .notDetermined:
+            let newStatus = await PHPhotoLibrary.requestAuthorization(for: .readWrite)
+            return newStatus == .authorized || newStatus == .limited
+
+        default:
+            return false
+        }
+    }
+
+    func fetchAssetCount() -> Int {
+        let assets = PHAsset.fetchAssets(with: nil)
+        return assets.count
+    }
+}
