29d56e385eaffaed992a9460121e502ee89cf78f
Tushar Mishra <thedynamic2508@gmail.com>
2026-06-02 14:31:24 -0400

    Added Published variables

--- PixelVault/AppState.swift
+++ PixelVault/AppState.swift
@@ -4,4 +4,15 @@
 //
 //  Created by Tushar Mishra on 6/2/26.
 //
+import Foundation
+import Combine
+import Foundation
 
+@MainActor
+final class AppState: ObservableObject {
+    @Published var statusText = "Ready"
+    @Published var pendingCount = 0
+    @Published var archivedCount = 0
+    @Published var lastUploadText = "Never"
+    @Published var isWorking = false
+}
